70f3cb609b945179cb24c6ca7b5b48c3d2d3a3ad
at3o <>
2022-03-16 16:24:45 +0100

    Formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
+import 'numeral/locales/it';
+numeral.locale('it');
 
 export const ExpenseListItem = ({id, description, amount, createdAt}) => (
   <div>
-    <Link to={`/edit/${id}`}><h3>Title: { description }</h3></Link>
-    <p>Amount: { amount } - CreatedAt: { createdAt }</p>
+    <Link to={`/edit/${id}`}>
+      <h3>Title: { description }</h3>
+    </Link>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('Do MMMM, YYYY')}
+    </p>
   </div>
 );
 
